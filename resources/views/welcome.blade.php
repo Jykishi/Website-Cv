8d7bfb85d997822e249eb60b3742f6b991418274
Jykishi <122472603+Jykishi@users.noreply.github.com>
2023-03-21 21:37:21 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">	<head>
+<body>
+    <!-- Container -->
+        <div class="container">
+            <div id="colspan">
+                <img src="{{asset('foto.png')}}">
+                <h3>JHONY SUSANTO</h3>
+                <p><i class="fa fa-briefcase fa-fw large"></i>QUALITY CONTROL</p>
+                <p><i class="fa fa-home fa-fw large"></i>BATAM, KEPULAUAN RIAU</p>
+                <p><i class="fa fa-phone fa-fw large"></i>081533432595</p>
+                <hr>
+                <h3><i class="fa fa-linux fa-fw large"></i>Skills</h3>
+                <p>HTML</p>
+                <div class="load">
+                    <div class="progress" style="width:90%">90%</div>
+                </div>
+                <p>CSS</p>
+                <div class="load2">
+                    <div class="progress2" style="width:80%">80%</div>
+                </div>
+                <p>Adobe Photoshop</p>
+                    <div class="load4">
+                    <div class="progress4" style="width:65%">65%</div>
+                </div>
+                <p>Adobe Premiere</p>
+                    <div class="load4">
+                    <div class="progress4" style="width:65%">65%</div>
+                </div>
+                <hr>
+                <h3><i class="fa fa-language fa-fw large"></i>Language</h3>
+                <p>English</p>
+                <p>Mandarin</p>
+                <hr>
+                <h3><i class="fa fa-globe fa-fw large"></i>Social Media</h3>
+                <a href="https://instagram.com/j_s.kn?igshid=ZDdkNTZiNTM="><i class="fa fa-instagram fa-fw large"></i></a>
+                <a href="https://github.com/Jhony691"><i class="fa fa-github fa-fw large"></i></a>
+                <a href="https://t.me/+6281533432595"><i class="fa fa-telegram fa-fw large"></i></a>
+                <a href="https://wa.me/qr/W73HRHVID6SYA1"><i class="fa fa-whatsapp fa-fw large"></i></a>
+                <a href="https://u.wechat.com/kBphRKPh8q6YatUjjlgTos0"><i class="fa fa-wechat fa-fw large"></i></a>
+            </div>
+          <div class="container-padding">
+        <div class="padding">
+                <div class="content1">
+            <h3>SMAN 1 KUNDUR</h3>
+            <p><i class="fa fa-calendar fa-fw large"></i>2019-2022</p>
+            <p>Majoring in Science
+            </p>
+            </div>
+            <hr>
+                <div class="content2"> 
+            <h3>University Internasional Batam</h3>
+            <p><i class="fa fa-calendar fa-fw large"></i>2022-Continue</p>
+            <p>Majoring in Information Systems
+            </p>
+            </div>
+            </div>
+            </div>
+          <div class="padding-bottom">
+                <div class="bottom">
+                 <div class="content1">
+            <h2><i class="fa fa-briefcase fa-fw large"></i>Experience :</h2>
+            <h3>working as a customer service at grace mobile</h3>
+            <p><i class="fa fa-calendar fa-fw large"></i>2021-2022</p>
+            </div>
+        </div>
+    </body>
+</html>
+<link rel="stylesheet" href="{{asset('style.css')}}">
